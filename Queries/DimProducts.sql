1a81d4bfdbf14df0dbabe0a1fb55aa3f80aa7147
Omar Akram <149201850+botta14@users.noreply.github.com>
2024-08-16 02:58:38 +0300

    Add files via upload
    This folder contains SQL scripts that define and manage the dimension tables used in the data warehouse for the SSIS project. Each script is responsible for creating and maintaining specific dimensions critical to the ETL process.

---
+++ Queries/DimProducts.sql
@@ -0,0 +1,16 @@
+create database Global_Superstore;
+
+
+use Global_Superstore;
+
+ALTER TABLE DimProducts
+ALTER COLUMN "Product ID" nvarchar(255) NOT NULL;
+
+ALTER TABLE DimProducts
+ADD CONSTRAINT PK_DimProducts PRIMARY KEY ("Product ID");
+
+alter table factOrders
+       add constraint fk_fact_orders_dim_product
+	   foreign key ("Product ID")
+	   references DimProducts("Product ID")
+;
---
+++ Queries/DimTime.sql
@@ -0,0 +1,13 @@
+use Global_Superstore;
+
+ALTER TABLE DimTime
+ALTER COLUMN TimeID int NOT NULL;
+
+ALTER TABLE DimTime
+ADD CONSTRAINT PK_DimTime PRIMARY KEY (TimeID);
+
+alter table factOrders
+       add constraint fk_fact_orders_dim_time
+	   foreign key (TimeID)
+	   references DimTime(TimeID)
+;
