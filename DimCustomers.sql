9ce234c64cfc0ad3aa4e2742cbea8a975097765f
Omar Akram <149201850+botta14@users.noreply.github.com>
2024-08-16 02:52:15 +0300

    Add files via upload

---
+++ DimCustomers.sql
@@ -0,0 +1,13 @@
+use Global_Superstore;
+
+ALTER TABLE DimCustomers
+ALTER COLUMN "Customer ID" nvarchar(255) NOT NULL;
+
+ALTER TABLE DimCustomers
+ADD CONSTRAINT PK_DimCustomers PRIMARY KEY ("Customer ID");
+
+alter table factOrders
+       add constraint fk_fact_orders_dim_customer
+	   foreign key ("Customer ID")
+	   references DimCustomers("Customer ID")
+;
---
+++ DimLocations.sql
@@ -0,0 +1,13 @@
+use Global_Superstore;
+
+ALTER TABLE DimLocations
+ALTER COLUMN LocationID int NOT NULL;
+
+ALTER TABLE DimLocations
+ADD CONSTRAINT PK_DimLocations PRIMARY KEY (LocationID);
+
+alter table factOrders
+       add constraint fk_fact_orders_dim_location
+	   foreign key (LocationID)
+	   references DimLocations(LocationID)
+;
